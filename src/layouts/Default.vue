da0fb4078c1b4496bef9bb2078edd045607e8b2e
AleCongi <somaticalexquad@gmail.com>
2024-05-29 21:09:12 +0200

    lateral menù

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,9 +1,25 @@
 <template>
   <v-app>
-    <default-view />
+    <v-card>
+      <v-layout>
+        <lateral-menu :modelValue="drawer" @update:modelValue="drawer = $event" />
+        <v-main>
+          <default-view />
+          <v-btn
+            icon="mdi-microsoft-xbox-controller-menu"
+            @click.stop="drawer = !drawer"
+            style="position: absolute; top: 16px; left: 16px; z-index: 3;"
+          />
+        </v-main>
+      </v-layout>
+    </v-card>
   </v-app>
 </template>
 
 <script setup>
-  import DefaultView from './View.vue';
+import DefaultView from './View.vue';
+import LateralMenu from './LateralMenu.vue';
+import { ref } from 'vue';
+
+const drawer = ref(false);
 </script>
---
+++ src/layouts/LateralMenu.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-navigation-drawer
+      :model-value="modelValue"
+      @update:model-value="$emit('update:modelValue', $event)"
+      temporary
+      style="z-index: 2;"
+    >
+      <v-list-item
+        prepend-icon="mdi-microsoft-xbox-controller-menu"
+        title="John Leider"
+      ></v-list-item>
+  
+      <v-divider></v-divider>
+  
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits } from 'vue';
+  
+  const props = defineProps({
+    modelValue: {
+      type: Boolean,
+      required: true,
+    },
+  });
+  
+  const emit = defineEmits(['update:modelValue']);
+  </script>
+  
