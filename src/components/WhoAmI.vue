7bcf5d424e68de0c263036669552711b781bd838
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-05-29 19:25:45 +0200

    setup

---
+++ src/components/WhoAmI.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <h1 :style="{ color: data.primaryColor }">
+      Chi sono
+    </h1>
+    <v-row>
+      <v-col cols="12" sm="4">
+        <center>
+          <v-avatar image="/avatar.jpeg" size="200" />
+        </center>
+      </v-col>
+      <v-col cols="12" sm="8" class="body_text_container">
+        <div>
+          <h1>Caterina</h1>
+          L'obiettivo che mi sono posta, aprendo questa casa vacanze, è quello di voler contribuire, nel mio piccolo, alla conoscenza della nostra regione e alla diffusione delle nostre tradizioni,
+          ospitando il maggior numero di persone. Nonostante quella dell'host non sia la mia principale attività, ho iniziato questa avventura con grande determinazione e Amore.
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+  import { storeToRefs } from 'pinia';
+  import { useDataStore } from '@/stores/data';
+
+  const dataStore = useDataStore();
+  const { data } = storeToRefs(dataStore);
+</script>
+
+<style scoped>
+  .body_text_container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <Gallery v-if="data.gallery" />
+  <WhoAmI id="whoAmI" />
+  <v-container v-if="data.contacts">
+    <hr :style="{ backgroundColor: data.primaryColor, height: '5px' }" />
+  </v-container>
+  <Services id="services" v-if="data.services" />
+  <v-container v-if="data.advantages">
+    <hr :style="{ backgroundColor: data.primaryColor, height: '5px' }" />
+  </v-container>
+  <Advantages id="advantages" v-if="data.advantages" />
+  <v-container v-if="data.contacts">
+    <hr :style="{ backgroundColor: data.primaryColor, height: '5px' }" />
+  </v-container>
+  <Contacts id="contacts" v-if="data.contacts" />
+  <Map v-if="data.map" />
+</template>
+
+<script setup>
+  import Map from '@/components/Map';
+  import WhoAmI from '@/components/WhoAmI';
+  import Gallery from '@/components/Gallery';
+  import Services from '@/components/Services';
+  import Contacts from '@/components/Contacts';
+  import Advantages from '@/components/Advantages';
+
+  import { storeToRefs } from 'pinia';
+  import { useDataStore } from '@/stores/data';
+
+  const dataStore = useDataStore();
+  const { data } = storeToRefs(dataStore);
+</script>
