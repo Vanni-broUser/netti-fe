284e587b76e0229c69f788a7f90d91c78bba128f
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-07-03 18:45:35 +0000

    Image Grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,30 @@
 <template>
-  <Carousel />
+  <div>
+    <Carousel mode="home" :player="false" :content="images" />
+  </div>
 </template>
 
 <script setup>
 import Carousel from '@/components/Carousel.vue';
+
+import immagine1 from '@/assets/817321E2-3ABC-453A-9336-C1A5A0DFDAAE-homepage.jpeg';
+import immagine2 from '@/assets/IMG_9243-homepage.jpg';
+import immagine3 from '@/assets/M_03-homepage.jpg';
+import immagine4 from '@/assets/Netti-10-bis.jpg';
+import immagine5 from '@/assets/PLANOVOLUMETRICO-SALV02.jpg';
+import immagine6 from '@/assets/Prova.jpg';
+import immagine7 from '@/assets/slide_1.jpg';
+import immagine8 from '@/assets/slide_2.jpg';
+import immagine9 from '@/assets/slide_6.jpg';
+
+const images = [
+  { src: immagine1 },
+  { src: immagine2 },
+  { src: immagine3 },
+  { src: immagine4 },
+  { src: immagine6 },
+  { src: immagine7 },
+  { src: immagine8 },
+  { src: immagine9 },
+];
 </script>
