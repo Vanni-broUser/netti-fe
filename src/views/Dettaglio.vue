cc652ce0144a4a788de1ee969d3effa7b8ea471d
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-07-21 23:33:30 +0200

    fix

--- src/views/Dettaglio.vue
+++ src/views/Dettaglio.vue
@@ -1,31 +1,21 @@
 <template>
-  <v-dialog v-model="dialog" max-width="80vw" persistent>
-    <v-card>
-      <Carousel mode="detail" :player="true" :content="images" />
-      <v-btn icon class="close-button">
-        <v-icon>mdi-close</v-icon>
-      </v-btn>
-    </v-card>
-  </v-dialog>
+  <Carousel mode="detail" :player="true" :content="images" />
+  <v-btn class="close-button" icon="mdi-close" />
 </template>
 
 <script setup>
-import { ref, watchEffect } from 'vue';
-import { useRouter } from 'vue-router';
 import Carousel from '@/components/Carousel.vue';
 
 const { id, content } = defineProps(['id', 'content']);
-const router = useRouter();
-const dialog = ref(false);
 
-import immagine1 from '@/assets/817321E2-3ABC-453A-9336-C1A5A0DFDAAE-homepage.jpeg';
-import immagine2 from '@/assets/IMG_9243-homepage.jpg';
-import immagine3 from '@/assets/M_03-homepage.jpg';
-import immagine4 from '@/assets/Netti-10-bis.jpg';
-import immagine6 from '@/assets/Prova.jpg';
-import immagine7 from '@/assets/slide_1.jpg';
-import immagine8 from '@/assets/slide_2.jpg';
-import immagine9 from '@/assets/slide_6.jpg';
+import immagine1 from '@/assets/home/817321E2-3ABC-453A-9336-C1A5A0DFDAAE-homepage.jpeg';
+import immagine2 from '@/assets/home/IMG_9243-homepage.jpg';
+import immagine3 from '@/assets/home/M_03-homepage.jpg';
+import immagine4 from '@/assets/home/Netti-10-bis.jpg';
+import immagine6 from '@/assets/home/Prova.jpg';
+import immagine7 from '@/assets/home/slide_1.jpg';
+import immagine8 from '@/assets/home/slide_2.jpg';
+import immagine9 from '@/assets/home/slide_6.jpg';
 
 const images = [
   { src: immagine1 },
@@ -37,15 +27,6 @@ const images = [
   { src: immagine8 },
   { src: immagine9 },
 ];
-
-watchEffect(() => {
-  // Monitoriamo il cambiamento dell'ID per aprire automaticamente il dialog
-  dialog.value = !!router.currentRoute.value.params.id;
-});
-
-function goToRicerche() {
-  router.push('/ricerche');
-}
 </script>
 
 <style scoped>
--- src/views/Progetti.vue
+++ src/views/Progetti.vue
@@ -3,15 +3,15 @@
 </template>
 
 <script setup>
-import immagine1 from '@/assets/817321E2-3ABC-453A-9336-C1A5A0DFDAAE-homepage.jpeg';
-import immagine2 from '@/assets/IMG_9243-homepage.jpg';
-import immagine3 from '@/assets/M_03-homepage.jpg';
-import immagine4 from '@/assets/Netti-10-bis.jpg';
-import immagine5 from '@/assets/PLANOVOLUMETRICO-SALV02.jpg';
-import immagine6 from '@/assets/Prova.jpg';
-import immagine7 from '@/assets/slide_1.jpg';
-import immagine8 from '@/assets/slide_2.jpg';
-import immagine9 from '@/assets/slide_6.jpg';
+import immagine1 from '@/assets/home/817321E2-3ABC-453A-9336-C1A5A0DFDAAE-homepage.jpeg';
+import immagine2 from '@/assets/home/IMG_9243-homepage.jpg';
+import immagine3 from '@/assets/home/M_03-homepage.jpg';
+import immagine4 from '@/assets/home/Netti-10-bis.jpg';
+import immagine5 from '@/assets/home/PLANOVOLUMETRICO-SALV02.jpg';
+import immagine6 from '@/assets/home/Prova.jpg';
+import immagine7 from '@/assets/home/slide_1.jpg';
+import immagine8 from '@/assets/home/slide_2.jpg';
+import immagine9 from '@/assets/home/slide_6.jpg';
 
 import { randomHeight } from '@/utils/gridUtils';
 
