64ca5a780ecbc1427ee91155029468fc1ac7cee6
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-08-24 22:48:26 +0200

    search

--- src/layouts/SearchCard.vue
+++ src/layouts/SearchCard.vue
@@ -1,19 +1,19 @@
 <template>
-  <v-card :title="$t('SearchCard.searchBoxTtile')">
+  <v-card :title="$t('SearchCard.searchBoxTitle')">
     <v-form @submit.prevent="searchPosts">
       <v-card-text>
         <v-slider v-model="searchType" step="1" max="2" :color="theme.current.value.primary" />
         <v-window v-model="searchType">
           <v-window-item>
-            {{ $t('SearchCard.titleCard1') }}<br><br>
+            {{ $t('SearchCard.titleCardText') }}<br><br>
             <v-text-field v-model="text" variant="outlined" />
           </v-window-item>
           <v-window-item>
-            {{ $t('SearchCard.titleCard2') }}<br><br>
+            {{ $t('SearchCard.titleCardYear') }}<br><br>
             <v-select v-model="year" variant="outlined" :items="getYears()" />
           </v-window-item>
           <v-window-item>
-            {{ $t('SearchCard.titleCard3') }}<br><br>
+            {{ $t('SearchCard.titleCardPlace') }}<br><br>
             <v-text-field v-model="place" variant="outlined" />
           </v-window-item>
         </v-window>
@@ -43,7 +43,11 @@
   };
 
   const searchPosts = () => {
-    if (searchType.value == 1)
+    if (searchType.value == 0)
+      location.href = `/ricerca-post?name=text&value=${encodeURIComponent(text.value)}`;
+    else if (searchType.value == 1)
       location.href = `/ricerca-post?name=year&value=${year.value}`;
+    else if (searchType.value == 2)
+      location.href = `/ricerca-post?name=place&value=${encodeURIComponent(place.value)}`;
   };
 </script>
--- src/views/RicercaPost.vue
+++ src/views/RicercaPost.vue
@@ -1,7 +1,10 @@
 <template>
   <div :class="{ margin: !isMobile }">
     <v-container>
-      <h2>{{ $t('SearchCard.titleCard2') }}: {{ route.query.value }}</h2>
+      <h2>
+        {{ $t(`SearchCard.titleCard${capitalize(route.query.name)}`) }}:
+        {{ route.query.value }}
+      </h2>
     </v-container>
     <ImageGrid v-if="loading" :content="posts" :numCols="3"/>
   </div>
@@ -20,13 +23,24 @@
   const loading = ref(false);
   const isMobile = setupMobileUtils();
 
-  http.getRequest('blog/post', {
-    'enrichment_name': route.query.name,
-    'enrichment_value': route.query.value
-  }, function (data) {
+  const args = {};
+  if (route.query.name == 'text')
+    args.title = route.query.value;
+  else {
+    args.enrichment_name = route.query.name;
+    args.enrichment_value = route.query.value;
+    if (route.query.name == 'place')
+      args.enrichment_like = true;
+  }
+
+  http.getRequest('blog/post', args, function (data) {
     posts.value = data.posts;
     loading.value = true;
   });
+
+  const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 </script>
 
 <style scoped>
