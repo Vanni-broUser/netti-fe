8cdece62c51652d04619e9021c662aa035d1a767
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-11-25 16:32:34 +0100

    fix fix

--- src/views/Ricerche.vue
+++ src/views/Ricerche.vue
@@ -13,7 +13,7 @@
       </v-col>
       <v-col cols="12" md="4">
         <router-link class="link" to="/extra">
-          <v-img :src="extra" style="height: 200px; width: 600px;" />
+          <v-img :src="extra" style="height: 200px; width: 560px;" />
           <br>
           <p style="color: black; font-size: x-large;">Extra</p>
           <p style="color: black;" v-for="n in 2">
