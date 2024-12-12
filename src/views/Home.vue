4f98126e1b534f3920f355c09621389e6e95c036
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2024-12-12 19:14:23 +0000

    Seo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,15 +3,19 @@
 </template>
 
 <script setup>
-  import Carousel from '@/components/Carousel.vue';
+import Carousel from '@/components/Carousel.vue';
+import { useHead } from '@vueuse/head';
+import { head } from "@/utils/SEO";
 
-  const images = [
-    '/home/M_03-homepage.jpg',
-    '/home/IMG_9243-homepage.jpg',
-    '/home/Netti-10-bis.jpg',
-    '/home/slide_2.jpg',
-    '/home/Prova.jpg',
-    '/home/slide_6.jpg',
-    '/home/slide_1.jpg'
-  ];
+const images = [
+  '/home/M_03-homepage.jpg',
+  '/home/IMG_9243-homepage.jpg',
+  '/home/Netti-10-bis.jpg',
+  '/home/slide_2.jpg',
+  '/home/Prova.jpg',
+  '/home/slide_6.jpg',
+  '/home/slide_1.jpg'
+];
+
+useHead(head);
 </script>
