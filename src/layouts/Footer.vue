ab37f69cc5942b428f8d9f1fecd9ba00197ba293
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-06-24 12:16:52 +0200

    fix-padding

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -1,14 +1,18 @@
 <template>
   <v-footer
     elevation="2"
-    style="min-height: 4vh; max-height: max-content;"
+    style="min-height: 4vh; max-height: max-content; padding-top: 0 !important; padding-bottom: 0 !important;"
   >
-    NETTI ARCHITETTI © {{ new Date().getFullYear() }} | Bari &mdash; Italia | studio@nettiarchitetti.it
+    <span v-if="isMobile">NETTI ARCHITETTI © {{ new Date().getFullYear() }}</span>
+    <span v-else>
+      NETTI ARCHITETTI © {{ new Date().getFullYear() }} | Bari &mdash; Italia | studio@nettiarchitetti.it
+    </span>
   </v-footer>
 </template>
 
 <script setup>
-//
+import { setupMobileUtils } from '@/utils/mobile';
+const isMobile = setupMobileUtils();
 </script>
 
 <style scoped>
