07e100734ca49298afd974e39c65bca3585bc469
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-02-11 15:44:38 +0100

    fix-portfolio

--- src/layouts/MenuOperaions.vue
+++ src/layouts/MenuOperaions.vue
@@ -5,20 +5,22 @@
         <div @click="setLanguage('it')" style="float: left; margin-left: 2px; cursor: pointer;">IT</div>
         <div @click="setLanguage('en')" style="float: left; margin-left: 20px; cursor: pointer;">EN</div>
         <br><br>
-        <div
-          class="d-flex align-center"
-        >
-          <router-link class="link" to="/portfolio-15" style="margin-right: 16px;">
-            <v-card width="143" elevation="5">
-              <v-img :src="portfolio15" />
-            </v-card>
-          </router-link>
-          <router-link class="link" to="/portfolio-25">
-            <v-card width="143" elevation="5">
-              <v-img :src="portfolio25" />
-            </v-card>
-          </router-link>
-        </div>
+        <v-row dense>
+          <v-col cols="12" sm="6">
+            <router-link class="link" to="/portfolio-15">
+              <v-card elevation="5">
+                <v-img :src="portfolio15" />
+              </v-card>
+            </router-link>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <router-link class="link" to="/portfolio-25">
+              <v-card elevation="5">
+                <v-img :src="portfolio25"  />
+              </v-card>
+            </router-link>
+          </v-col>
+        </v-row>
         <br>
         <div v-bind="activatorProps" style="cursor: pointer;">
           {{ $t('MenuOperations.searchButton') }}
@@ -38,9 +40,6 @@ import portfolio15 from '@/assets/portfolio15.png';
 import portfolio25 from '@/assets/portfolio25.png';
 
 import SearchCard from './SearchCard';
-import { setupMobileUtils } from '@/utils/mobile';
-
-const isMobile = setupMobileUtils();
 
 const setLanguage = (language) => {
   i18n.global.locale = language;
