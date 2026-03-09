7e6b6e089929616843ca91677e9646e14cee8513
Vanni <hosting23232323@gmail.com>
2026-03-09 11:53:27 +0000

    New download

--- src/views/Portfolio25.vue
+++ src/views/Portfolio25.vue
@@ -1,11 +1,4 @@
 <template>
-  <v-btn
-    variant="text"
-    color="white"
-    icon="mdi-download"
-    class="background-red square-btn main-button"
-    @click="downloadPDF('/portfolio-25.pdf')"
-  />
   <PortfolioPageViewer :pdf="'/portfolio-25.pdf'" v-if="!isMobile" />
   <PortfolioFullViewer :pdf="'/portfolio-25.pdf'" v-else />
 </template>
@@ -16,27 +9,4 @@ import PortfolioPageViewer from '@/components/PortfolioPageViewer.vue';
 import PortfolioFullViewer from '@/components/PortfolioFullViewer.vue';
 
 const isMobile = setupMobileUtils();
-const downloadPDF = (pdfUrl) => {
-  const link = document.createElement('a');
-  link.href = pdfUrl;
-  link.download = pdfUrl.split('/').pop();
-  document.body.appendChild(link);
-  link.click();
-  document.body.removeChild(link);
-};
 </script>
-
-<style scoped>
-.square-btn {
-  width: 25px;
-  height: 25px;
-  border-radius: 0px;
-}
-
-.main-button {
-  position: fixed;
-  right: 15px;
-  top: 45px;
-  z-index: 999;
-}
-</style>
